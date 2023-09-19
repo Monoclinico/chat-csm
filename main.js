a9cb1e1144213cb749a4ae04414081e4132e7eaf
Matheus Sanches <matheussanchesleme@gmail.com>
2023-09-19 01:02:52 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ const typebotInitScript = document.createElement("script");
 typebotInitScript.type = "module";
 typebotInitScript.innerHTML = `import Typebot from 'https://cdn.jsdelivr.net/npm/@typebot.io/js@0.1/dist/web.js'
   
-Typebot.initStandard({ typebot: "avon-chat-consumidor" });
+Typebot.initStandard({ typebot: "avon-chat-csm-1" });
 `;
 document.body.append(typebotInitScript);
