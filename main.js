716c436df7418bb44bab09fde8bb711f6b18e1ce
Matheus Sanches <matheussanchesleme@gmail.com>
2023-09-19 23:47:39 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
+const d = new Date();
+let seconds = d.getSeconds().valueOf();
 const typebotInitScript = document.createElement("script");
 typebotInitScript.type = "module";
-typebotInitScript.innerHTML = `import Typebot from 'https://cdn.jsdelivr.net/npm/@typebot.io/js@0.1/dist/web.js'
+
+
+if (seconds  % 2 == 0) {
+    //par
+    typebotInitScript.innerHTML = `import Typebot from 'https://cdn.jsdelivr.net/npm/@typebot.io/js@0.1/dist/web.js'
   
-Typebot.initStandard({ typebot: "avon-chat-csm-1" });
-`;
+Typebot.initStandard({ typebot: "avon-chat-csm-1" });`;
+}else{
+    //impar
+    typebotInitScript.innerHTML = `import Typebot from 'https://cdn.jsdelivr.net/npm/@typebot.io/js@0.1/dist/web.js'
+  
+    Typebot.initStandard({ typebot: "avon-chat-csm-2" });`;
+}
+
 document.body.append(typebotInitScript);
